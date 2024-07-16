0d6d76aecfc38c6c51e932dd660aaeecc9f30eed
Ana Luiz <anamurdock881@gmail.com>
2024-07-15 23:48:49 -0300

    refactor: input import

--- frontend/src/app/components/Input/index.tsx
+++ frontend/src/app/components/Input/index.tsx
@@ -1,7 +1,3 @@
 import { Input } from "./fields/Input";
 
-import { Default } from "./fields/Default";
-import { Currency } from "./fields/Currency";
-import { Select } from "./fields/Select";
-
-export { Input, Default, Currency, Select };
+export { Input };
